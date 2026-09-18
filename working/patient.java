a2cde472a98f226bc42c87ee045ee61ee43ee9e7
shriya-m-05 <mistryshriya05@gmail.com>
2026-09-18 22:16:24 +0530

    Refactor Patient class to use shorter variable names

--- working/patient.java
+++ working/patient.java
@@ -5,33 +5,33 @@ import java.util.Scanner;
 
 public class Patient 
 {
-    private Connection connection;
-    private Scanner scanner;
+    private Connection c;
+    private Scanner sc;
 
-    public Patient(Connection connection, Scanner scanner)
+    public Patient(Connection c, Scanner sc)
     {
-        this.connection = connection;
-        this.scanner = scanner;
+        this.c = c;
+        this.sc = sc;
     }
 
     public void addPatient()
     {
         System.out.print("Enter Patient Name: ");
-        String name = scanner.next();
+        String name = sc.next();
         System.out.print("Enter Patient Age: ");
-        int age = scanner.nextInt();
+        int age = sc.nextInt();
         System.out.print("Enter Patient Gender: ");
-        String gender = scanner.next();
+        String gender = sc.next();
 
         try
         {
             String query = "INSERT INTO patients(name, age, gender) VALUES(?, ?, ?)";
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            PreparedStatement preparedStatement = c.prepareStatement(query);
             preparedStatement.setString(1, name);
             preparedStatement.setInt(2, age);
             preparedStatement.setString(3, gender);
             int affectedRows = preparedStatement.executeUpdate();
-            if(affectedRows>0)
+            if(affectedRows > 0)
                 System.out.println("Patient Added Successfully!!");
             else
                 System.out.println("Failed to add Patient!!");
@@ -48,12 +48,12 @@ public class Patient
         String query = "select * from patients";
         try
         {
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            PreparedStatement preparedStatement = c.prepareStatement(query);
             ResultSet resultSet = preparedStatement.executeQuery();
             System.out.println("Patients: ");
-            System.out.println("-----------------------------------------------------------");
+            System.out.println("+------------+--------------------+----------+------------+");
             System.out.println("| Patient Id | Name               | Age      | Gender     |");
-            System.out.println("-----------------------------------------------------------");
+            System.out.println("+------------+--------------------+----------+------------+");
             while(resultSet.next())
             {
                 int id = resultSet.getInt("id");
@@ -76,7 +76,7 @@ public class Patient
         String query = "SELECT * FROM patients WHERE id = ?";
         try
         {
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            PreparedStatement preparedStatement = c.prepareStatement(query);
             preparedStatement.setInt(1, id);
             ResultSet resultSet = preparedStatement.executeQuery();
             if(resultSet.next())
